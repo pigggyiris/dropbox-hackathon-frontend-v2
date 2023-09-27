385d330ac55f291fdde9d475e6258725b76bae21
pigggyiris <95093482+pigggyiris@users.noreply.github.com>
2023-09-26 21:16:48 -0400

    Update SinglePetition.js

--- src/pages/SinglePetition.js
+++ src/pages/SinglePetition.js
@@ -1,23 +1,85 @@
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import dummyPetitionData from "../components/dummyPetitionData";
-import pdfFile from '../assets/Signatures.pdf';
-
+import pdfFile from "../assets/Signatures.pdf";
+import axios from "axios";
+import HelloSign from "hellosign-embedded";
 
 function SinglePetitionPage() {
   const { fileID } = useParams();
-  const File = dummyPetitionData.find((d) => {
-    return d.id === parseInt(fileID);
-  });
-  const percentage = Math.round((File.signed.length / File.target) * 100);
+  const [petition, setPetition] = useState(null);
+  const client = new HelloSign();
+  const [petitionId, setPetitionId] = useState(null);
+  const [signId, setSignId] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/v1/petitions/${fileID}`)
+      .then((response) => {
+        setPetition(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching petition:", error);
+      });
+  }, [fileID]);
+
+  function handleSign() {
+    axios
+      .put(`http://localhost:3000/v1/petitions/${fileID}`)
+      .then((response) => {
+        console.log("Petition signed successfully", response.data);
+        if (response.data && response.data.signUrl) {
+          const signUrl = response.data.signUrl;
+
+          setPetitionId(response.data.petitionId);
+          setSignId(response.data.signId);
+
+          client.open(signUrl, {
+            clientId: "efe3f85f67aafe39ff9adc714413cf11",
+            skipDomainVerification: true,
+          });
+        } else {
+          console.log("No signUrl provided in response");
+        }
+      })
+      .catch((error) => {
+        console.error("Error signing petition:", error);
+      });
+  }
+
+  const handleFinishSignClick = async () => {
+    if (!petitionId || !signId) {
+      console.error("Missing petitionId or signId");
+      return;
+    }
+
+    try {
+      const response = await axios.put("http://localhost:3000/v1/petitions/", {
+        petitionId: petitionId,
+        signId: signId,
+      });
+      console.log("Petition updated successfully!", response.data);
+    } catch (error) {
+      console.error("Error updating petition:", error);
+    }
+  };
+
+  if (!petition) {
+    return <div>Loading...</div>;
+  }
+  let signedCount = petition?.signed?.length || 0;
+
+  const percentage = Math.round((signedCount / petition.target) * 100);
 
   return (
     <div className="flex">
       <div className="w-3/5 m-10">
-        <img src={File.image} alt="" className="w-full rounded-lg mb-4" />
-        <h1 className="font-bold text-xl mb-2">{File.title}</h1>
-        <p className="text-gray-600 mb-2">{"Created by: " + File.author}</p>
-        <p className="text-gray-600 mb-2">{"Created On: " + File.createdOn}</p>
-        <p className="line-clamp-5 mb-2">{File.description}</p>
+        <img src={petition.image} alt="" className="w-full rounded-lg mb-4" />
+        <h1 className="font-bold text-xl mb-2">{petition.title}</h1>
+        <p className="text-gray-600 mb-2">{"Created by: " + petition.author}</p>
+        <p className="text-gray-600 mb-2">
+          {"Created On: " + petition.createdOn}
+        </p>
+        <p className="line-clamp-5 mb-2">{petition.description}</p>
       </div>
 
       <div className="w-2/5 m-10 p-10 border border-gray-300 rounded-xl">
@@ -31,39 +93,91 @@ function SinglePetitionPage() {
               </span>
             </div>
             <div className="text-right">
-            <span className="text-xs font-semibold inline-block">
-              {File.signed.length + "/" + File.target} 
-              <a href={pdfFile} className="text-blue-500 hover:text-blue-700 text-xs ml-2" target="_blank" rel="noopener noreferrer">
+              <span className="text-xs font-semibold inline-block">
+                {signedCount + "/" + petition.target}
+                <a
+                  href={pdfFile}
+                  className="text-blue-500 hover:text-blue-700 text-xs ml-2"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Signatures
-              </a>
-            </span>
-          </div>
-
+                </a>
+              </span>
+            </div>
           </div>
           <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-teal-200">
-            <div style={{ width: `${percentage}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-600"></div>
+            <div
+              style={{ width: `${percentage}%` }}
+              className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-600"
+            ></div>
           </div>
         </div>
 
         <div className="mb-4">
-          <label className="block text-sm font-semibold mb-2" htmlFor="email">Email</label>
-          <input type="email" id="email" className="w-full p-2 border rounded-md" placeholder="Enter your email" />
+          <label className="block text-sm font-semibold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="w-full p-2 border rounded-md"
+            placeholder="Enter your email"
+          />
         </div>
         <div className="flex mb-4">
           <div className="mr-2 w-1/2">
-            <label className="block text-sm font-semibold mb-2" htmlFor="lastName">LastName</label>
-            <input type="text" id="lastName" className="w-full p-2 border rounded-md" placeholder="Enter your last name" />
+            <label
+              className="block text-sm font-semibold mb-2"
+              htmlFor="lastName"
+            >
+              LastName
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              className="w-full p-2 border rounded-md"
+              placeholder="Enter your last name"
+            />
           </div>
           <div className="ml-2 w-1/2">
-            <label className="block text-sm font-semibold mb-2" htmlFor="firstName">FirstName</label>
-            <input type="text" id="firstName" className="w-full p-2 border rounded-md" placeholder="Enter your first name" />
+            <label
+              className="block text-sm font-semibold mb-2"
+              htmlFor="firstName"
+            >
+              FirstName
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              className="w-full p-2 border rounded-md"
+              placeholder="Enter your first name"
+            />
           </div>
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-semibold mb-2" htmlFor="phone">Phone Number (Optional)</label>
-          <input type="text" id="phone" className="w-full p-2 border rounded-md" placeholder="Enter your phone number (optional)" />
+          <label className="block text-sm font-semibold mb-2" htmlFor="phone">
+            Phone Number (Optional)
+          </label>
+          <input
+            type="text"
+            id="phone"
+            className="w-full p-2 border rounded-md"
+            placeholder="Enter your phone number (optional)"
+          />
         </div>
-        <button className="py-3 px-6">Sign</button>
+
+        <div className="flex">
+          <button onClick={handleSign} className="py-3 px-6 mr-2">
+            Sign
+          </button>
+          <button
+            onClick={handleFinishSignClick}
+            className="py-3 px-6 bg-teal-500 text-gray-50"
+          >
+            Finish Sign
+          </button>
+        </div>
       </div>
     </div>
   );
